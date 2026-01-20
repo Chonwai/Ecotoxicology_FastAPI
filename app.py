105020e1dd78719a1690aeae89960392ee3a3fdb
Chonwai <chonwaiun@gmail.com>
2026-01-20 11:37:03 +0800

    fix: update FastAPI server port to 8890

--- app.py
+++ app.py
@@ -248,4 +248,4 @@ async def predict(request: PredictRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8888)
+    uvicorn.run(app, host="0.0.0.0", port=8890)
